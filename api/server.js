9f963bd3f29403ad5b3e30a634e912ffca04107c
Nathaniel Patterson <odst0016@gmail.com>
2020-09-27 14:45:00 -0400

    test

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const helmet = require('helmet');
-
-const apiRouter = require('./api-router.js');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const apiRouter = require("./api-router.js");
 
 const server = express();
-
+server.use(cors());
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
 module.exports = server;
